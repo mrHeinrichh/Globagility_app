35808381f72a557230347597d470064c2cba16de
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2023-11-13 14:19:55 +0800

    feat: :lipstick: Add time entry page user interface

---
+++ lib/views/time_entry.page.dart
@@ -0,0 +1,17 @@
+// time_entry_page.dart
+
+import 'package:flutter/material.dart';
+
+class TimeEntryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Time Entry Page'),
+      ),
+      body: Center(
+        child: Text('Time Entry Content Goes Here'),
+      ),
+    );
+  }
+}
