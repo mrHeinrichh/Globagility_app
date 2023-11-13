41aeeda7809a186af8ef635606a949aecbc06326
Heinrich Fabros <johnheinrich.fabros@tup.edu.ph>
2023-11-13 15:17:27 +0800

    feat: :lipstick: Update welcome page interface

--- lib/views/welcome.page.dart
+++ lib/views/welcome.page.dart
@@ -1,5 +1,3 @@
-// welcome_page.dart
-
 import 'package:flutter/material.dart';
 import 'package:globagility_app/views/leave.page.dart';
 import 'package:globagility_app/views/official_business.page.dart';
@@ -51,18 +49,74 @@ class WelcomePage extends StatelessWidget {
                 );
               },
             ),
-            HyperlinkWidget(
-              text: 'My Time Entry',
+            GestureDetector(
               onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => TimeEntryPage()),
-                );
+                _showMyTimeEntryModal(context);
               },
+              child: Text(
+                'My Time Entry',
+                style: TextStyle(
+                  color: Colors.blue, // You can customize the text color
+                  decoration: TextDecoration.underline,
+                ),
+              ),
             ),
           ],
         ),
       ),
+    );
+  }
+
+  void _showMyTimeEntryModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              DropdownButton<String>(
+                items: [
+                  DropdownMenuItem<String>(
+                    child: Text('Organization 1'),
+                    value: 'Organization 1',
+                  ),
+                  DropdownMenuItem<String>(
+                    child: Text('Organization 2'),
+                    value: 'Organization 2',
+                  ),
+                  DropdownMenuItem<String>(
+                    child: Text('Organization 3'),
+                    value: 'Organization 3',
+                  ),
+                ],
+                onChanged: (String? value) {
+                  // Handle organization change
+                },
+                hint: Text('Select an Organization'),
+                isExpanded: true,
+              ),
+              SizedBox(height: 16),
+              TextField(
+                decoration: InputDecoration(
+                  labelText: 'Please input Employee ID',
+                ),
+              ),
+              SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TimeEntryPage()),
+                  );
+                },
+                child: Text('Proceed'),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
